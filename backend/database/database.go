1bf15cf8bea3e1242071fc36fc458ad9b4f3d658
ardi-mahardika <dikaredarmy2@gmail.com>
2026-03-12 12:02:59 +0700

    Update production database to Aiven

--- backend/database/database.go
+++ backend/database/database.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"log"
+	"os"
+	"steam-motor-backend/models"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -10,15 +12,25 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	// DSN format: user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local
-	// For demo purposes, assuming root with no password on localhost. We will configure this.
-	// We use steammotor_db created earlier.
-	dsn := "root:@tcp(127.0.0.1:3306)/steammotor_db?charset=utf8mb4&parseTime=True&loc=Local"
+	// Let's use an environment variable for the production Cloud Database
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		// Local development fallback
+		dsn = "root:@tcp(127.0.0.1:3306)/steammotor_db?charset=utf8mb4&parseTime=True&loc=Local"
+	}
+
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database. \n", err)
 	}
 
+	// Migrate the schema (Auto-creates tables if they don't exist)
+	err = db.AutoMigrate(&models.User{}, &models.Transaction{}, &models.Expense{}, &models.Petugas{})
+	if err != nil {
+		log.Fatal("Failed to migrate database schema. \n", err)
+	}
+
 	DB = db
 	log.Println("Database connection successfully opened")
 }
+
