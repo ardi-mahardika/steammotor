bbef7dbaafdbb0243830175efd034e7d855a3273
ardi-mahardika <dikaredarmy2@gmail.com>
2026-03-12 14:46:58 +0700

    Fix DATABASE_URL parsing for Aiven mysql:// format

--- backend/database/database.go
+++ backend/database/database.go
@@ -1,9 +1,12 @@
 package database
 
 import (
+	"fmt"
 	"log"
+	"net/url"
 	"os"
 	"steam-motor-backend/models"
+	"strings"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -12,11 +15,27 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	// Let's use an environment variable for the production Cloud Database
-	dsn := os.Getenv("DATABASE_URL")
-	if dsn == "" {
+	var dsn string
+
+	rawURL := os.Getenv("DATABASE_URL")
+	if rawURL == "" {
 		// Local development fallback
 		dsn = "root:@tcp(127.0.0.1:3306)/steammotor_db?charset=utf8mb4&parseTime=True&loc=Local"
+	} else {
+		// Convert mysql:// URL format to GORM DSN format
+		// Input:  mysql://user:pass@host:port/dbname?ssl-mode=REQUIRED
+		// Output: user:pass@tcp(host:port)/dbname?charset=utf8mb4&parseTime=True&tls=true
+		rawURL = strings.Replace(rawURL, "mysql://", "http://", 1)
+		u, err := url.Parse(rawURL)
+		if err != nil {
+			log.Fatal("Invalid DATABASE_URL format: ", err)
+		}
+		password, _ := u.User.Password()
+		host := u.Hostname()
+		port := u.Port()
+		dbName := strings.TrimPrefix(u.Path, "/")
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local&tls=true",
+			u.User.Username(), password, host, port, dbName)
 	}
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -33,4 +52,3 @@ func ConnectDB() {
 	DB = db
 	log.Println("Database connection successfully opened")
 }
-
